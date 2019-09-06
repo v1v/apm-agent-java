9772bfb4279fb6e7b59ea666851ed1f99fb6abad
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-09-06 22:33:14 +0100

    fix: --fail-never  seems to even fail the build

--- .ci/packer_cache.sh
+++ .ci/packer_cache.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source /usr/local/bin/bash_standard_lib.sh
 
-JAVA_HOME=$HOME/.java/java10 ./mvnw clean verify --fail-never
+JAVA_HOME=$HOME/.java/java10 MAVEN_OPTS=-Dorg.slf4j.simpleLogger.defaultLogLevel=warn ./mvnw --batch-mode clean verify || true
 
 (retry 2 docker pull docker.elastic.co/observability-ci/weblogic:12.2.1.3-dev) \
   && docker tag docker.elastic.co/observability-ci/weblogic:12.2.1.3-dev store/oracle/weblogic:12.2.1.3-dev
